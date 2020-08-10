bb020b19fc8c6d31237ad3af3cee396add5ea938
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2020-08-10 13:53:52 +0200

    Add 'accept_legacy' command line parameter
    ... to avoid looking for PCI serial controllers and use the legacy COM
    ports. This is useful if you only want the module relocation support.

--- standalone/bender.c
+++ standalone/bender.c
@@ -8,8 +8,17 @@
 #include <serial.h>
 #include <bda.h>
 
-/* Configuration (set by command line parser) */
+/*
+ * Configuration (set by command line parser)
+ */
+
+/* If true, accepts any device with SIMPLE_COMM class, not just serial controllers. This might do the wrong thing. */
 static bool be_promisc = false;
+
+/* Don't look for PCI serial controllers, if the BIOS indicates that we have legacy COM ports. */
+static bool accept_legacy = false;
+
+/* Upper limit of where we are willing to relocate modules. */
 static uint64_t phys_max_relocate = 1ULL << 31; /* below 2G */
 
 void
@@ -32,9 +41,11 @@ parse_cmdline(const char *cmdline)
 
     if (strcmp(token, "promisc") == 0) {
       be_promisc = true;
-    }
-    if (strcmp(token, "phys_max=256M") == 0)
+    } else if (strcmp(token, "phys_max=256M") == 0) {
       phys_max_relocate = 256ULL * 1024 * 1024;
+    } else if (strcmp(token, "accept_legacy") == 0) {
+      accept_legacy = true;
+    }
   }
 }
 
@@ -64,9 +75,18 @@ main(uint32_t magic, struct mbi *mbi)
   printf("\nBender %s", version_str);
   printf("Looking for serial controllers on the PCI bus...\n");
 
+  printf("Promisc is %s.\n", be_promisc ? "on" : "off");
+
+  uint16_t *com0_port      = (uint16_t *)(get_bios_data_area());
+  uint16_t *equipment_word = &get_bios_data_area()->equipment;
+
+  if (accept_legacy && ((*equipment_word & 0x7) != 0)) {
+    printf("Found legacy serial. Skipping PCI discovery.\n");
+    goto boot_next;
+  }
+
   struct pci_device serial_ctrl;
 
-  printf("Promisc is %s.\n", be_promisc ? "on" : "off");
   if (pci_find_device_by_class(PCI_CLASS_SIMPLE_COMM,
 			       be_promisc ? PCI_SUBCLASS_ANY : PCI_SUBCLASS_SERIAL_CTRL,
 			       &serial_ctrl)) {
@@ -86,9 +106,6 @@ main(uint32_t magic, struct mbi *mbi)
   }
 
   iobase = apply_quirks(&serial_ctrl, iobase);
-
-  uint16_t *com0_port      = (uint16_t *)(get_bios_data_area());
-  uint16_t *equipment_word = &get_bios_data_area()->equipment;
 
   if (iobase != 0) {
     printf("Patching BDA with I/O port 0x%x.\n", iobase);
