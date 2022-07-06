c5c8d36a761e59882cd550460e90134875074dab
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-07-06 10:13:03 +0200

    amy: probe for legacy COM ports

--- standalone/amy.c
+++ standalone/amy.c
@@ -4,11 +4,36 @@
 #include <util.h>
 #include <mbi.h>
 #include <version.h>
+#include <serial.h>
+#include <bda.h>
 
 static void
 parse_cmdline(const char *cmdline)
 {
   /* Nothing here yet. */
+}
+
+static void
+probe_legacy(void)
+{
+  struct bios_data_area *bda = get_bios_data_area();
+
+  for (size_t i = 0; i < ARRAY_SIZE(bda->com_port); i++) {
+    uint16_t port = bda->com_port[i];
+
+    /* At least on Qemu unused COM ports have a port number of 0. Also misaligned port numbers are bogus. */
+    if (port == 0 || ((port & 0x7) != 0)) {
+      printf("Skipping COM%zu at port %x.\n", i + 1, port);
+      continue;
+    }
+
+    serial_init_port(port);
+
+    /* COM port numbers start at 1. */
+    printf("Probing COM%zu at port %x...\n", i + 1, port);
+
+    serial_disable();
+  }
 }
 
 int
@@ -24,6 +49,8 @@ main(uint32_t magic, struct mbi *mbi)
 
   printf("\nAmy %s", version_str);
   printf("Probing for serial controllers...\n");
+
+  probe_legacy();
 
   printf("Our job is done. Sleeping.\n");
   cli_halt();
