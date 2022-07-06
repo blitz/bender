73cce47058abf33b9d77584be1e4e81b79c0e001
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-07-06 10:20:18 +0200

    pci: cleanup whitespace issues

--- standalone/pci.c
+++ standalone/pci.c
@@ -55,7 +55,7 @@ populate_device_info(uint32_t cfg_address, struct pci_device *dev)
   uint32_t vendor_id = pci_read_uint32(cfg_address + PCI_CFG_VENDOR_ID);
   uint32_t device_id = vendor_id >> 16;
   vendor_id &= 0xFFFF;
-  
+
   dev->db = pci_lookup_device(vendor_id, device_id);
   dev->cfg_address = cfg_address;
 }
@@ -63,7 +63,7 @@ populate_device_info(uint32_t cfg_address, struct pci_device *dev)
 
 bool
 pci_find_device_by_class(uint8_t class, uint8_t subclass,
-			 struct pci_device *dev)
+                         struct pci_device *dev)
 {
   uint32_t res = 0;
   uint16_t full_class = class << 8 | subclass;
@@ -73,15 +73,15 @@ pci_find_device_by_class(uint8_t class, uint8_t subclass,
 
   for (unsigned i=0; i<1<<13; i++) {
     uint8_t maxfunc = 0;
-    
+
     for (unsigned func = 0; func <= maxfunc; func++) {
       uint32_t addr = 0x80000000 | i<<11 | func<<8;
 
       if (!maxfunc && pci_read_uint8(addr+14) & 0x80)
-	maxfunc=7;
+        maxfunc=7;
 
       if ((full_class & class_mask) == ((pci_read_uint32(addr+0x8) >> 16) & class_mask))
-	res = addr;
+        res = addr;
     }
   }
 
@@ -89,7 +89,7 @@ pci_find_device_by_class(uint8_t class, uint8_t subclass,
     populate_device_info(res, dev);
     return true;
   } else {
-    return false;    
+    return false;
   }
 }
 
