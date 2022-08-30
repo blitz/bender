ec7dbdeb3f7c98809efa6c365d8cf727c670c281
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-08-30 16:21:29 +0300

    shell: add common tools to developer shell

--- flake.nix
+++ flake.nix
@@ -12,23 +12,31 @@
 
   outputs = { self, nixpkgs, flake-compat }:
     let
-      bender = nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix/build.nix {};
+      bender =
+        nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix/build.nix { };
     in {
       overlays.default = import ./nix/overlay.nix;
 
       packages.x86_64-linux = {
         inherit bender;
-        bender-debug = bender.override {
-          buildType = "Debug";
-        };
+        bender-debug = bender.override { buildType = "Debug"; };
 
         default = bender;
       };
 
       checks.x86_64-linux = {
-        bender-tests = nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix/tests.nix {
-          inherit bender;
-        };
+        bender-tests =
+          nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix/tests.nix {
+            inherit bender;
+          };
+      };
+
+      devShell.x86_64-linux = let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        inputsFrom = [ self.packages.x86_64-linux.default ];
+        packages = [ pkgs.cmakeCurses pkgs.nixfmt ];
+
+        hardeningDisable = [ "all" ];
       };
     };
 }
