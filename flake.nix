adb27d6f949f7238b355667be2a54edc6ba1b6d4
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2023-01-05 14:40:19 +0100

    ci: switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -11,51 +11,62 @@
       url = "github:cyberus-technology/hedron";
       flake = false;
     };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-compat, hedronSrc }:
-    let
-      bender =
-        nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix/build.nix { };
-      bender-debug = bender.override { buildType = "Debug"; };
-    in {
-      overlays.default = import ./nix/overlay.nix;
+  outputs = inputs@{ self, nixpkgs, flake-compat, hedronSrc, flake-parts }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
 
-      packages.x86_64-linux = {
-        inherit bender bender-debug;
+      perSystem = { config, pkgs, ... }: {
+        packages = rec {
+          bender = pkgs.callPackage ./nix/build.nix { };
+          bender-debug = bender.override { buildType = "Debug"; };
 
-        default = bender;
-      };
+          default = bender;
+        };
 
-      checks.x86_64-linux = let
-        # Hedron is not a flake. If we import its default.nix, this
-        # fails, because it relies on impure evaluation. We work
-        # around this issue by passing our nixpkgs along.
-        hedron = (import "${hedronSrc}/nix/release.nix" {
-          sources = null;
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        }).hedron.builds.default-release;
-      in {
-        bender-tests =
-          nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix/tests.nix {
-            inherit bender hedron;
+        checks =
+          let
+            # Hedron is not a flake. If we import its default.nix, this
+            # fails, because it relies on impure evaluation. We work
+            # around this issue by passing our nixpkgs along.
+            hedron = (import "${hedronSrc}/nix/release.nix" {
+              sources = null;
+              inherit pkgs;
+            }).hedron.builds.default-release;
+          in
+          {
+            bender-tests =
+              pkgs.callPackage ./nix/tests.nix {
+                inherit hedron;
+                bender = config.packages.bender;
+              };
+
+            bender-tests-debug =
+              pkgs.callPackage ./nix/tests.nix {
+                inherit hedron;
+
+                bender = config.packages.bender-debug;
+              };
           };
 
-        bender-tests-debug =
-          nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix/tests.nix {
-            inherit hedron;
-
-            bender = bender-debug;
-          };
-      };
-
-      devShells.x86_64-linux.default =
-        let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        in pkgs.mkShell {
-          inputsFrom = [ self.packages.x86_64-linux.default ];
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ config.packages.bender ];
           packages = [ pkgs.cmakeCurses pkgs.nixfmt ];
 
           hardeningDisable = [ "all" ];
         };
+      };
+
+      flake = {
+        overlays.default = import ./nix/overlay.nix;
+      };
     };
 }
